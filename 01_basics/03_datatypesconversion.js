15ae87556cba2df3aa9abe2bf52955a89868ec64
Shubham Kumar Yadav <skylife29974@gmail.com>
2024-08-03 06:14:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypesconversion.js
@@ -0,0 +1,17 @@
+let score = "shubham"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//OTHER EXAMPLE
+
+let a = 1
+
+let changein = Boolean(a)
+console.log(changein);
+console.log(typeof changein);
+
