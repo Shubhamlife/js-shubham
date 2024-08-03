20886f96c8fad5b556822b25eecafdcb759462de
Shubham Kumar Yadav <skylife29974@gmail.com>
2024-08-03 06:13:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";// treat all JS code as a newer -> but nor necessary to write this now 
+
+let state; // this is undefined where value have not been assigned yet.
+
+// whereas null = standalone value means that the output is null not zero
+
+//Symbol this is used to uniquely identify the value and separate it from other.
+
+console.log(typeof "shuhbham"); // this is string as it is between ""
+
+console.log(typeof null)// it is an object 
+console.log(typeof undefined) // it is undefined only 
