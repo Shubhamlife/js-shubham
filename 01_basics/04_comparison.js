bee6c179e80e0027d0368aa3cb449b20c553817c
Shubham Kumar Yadav <skylife29974@gmail.com>
2024-08-07 16:18:25 +0000

    comparising datatype.

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// In this the number which is written as a string is converted into a number and then it is compared and gives a output.
+
+console.log("2">1);
+console.log("02">1);
+
+// Here "==" in this operator treated differently as compare to other operators like <,>,<=,>=.
+// The "Null" is  converted into "0"
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// Here in this "===" no conversion of datatype is done. It strictly checks the datatype in its original form.
+
+console.log("2"===2);
